13dac65a1ff2277841940a0098951edd2b9d0fee
anim710 <anim3654@gmail.com>
2026-05-02 22:17:46 +0600

    database connection resolved

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,6 +3,10 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
+// console.log(process.env.MONGODB_URI);
+// console.log("Connecting to MongoDB...");
+// await client.connect();
+console.log("Connected to MongoDB!");
 const db = client.db("skillsphere");
 
 export const auth = betterAuth({
