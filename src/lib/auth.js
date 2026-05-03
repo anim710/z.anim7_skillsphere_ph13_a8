1bd2c29e615fec006ecb3d2eed553f4519935e19
anim710 <anim3654@gmail.com>
2026-05-03 23:27:44 +0600

    added session object in auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -37,9 +37,14 @@ export const auth = betterAuth({
     enabled: true,
   },
   socialProviders: {
+    
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
+  session: {
+        expiresIn: 60 * 60 * 24 * 7, // 7 days
+        updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
+    }
 });
